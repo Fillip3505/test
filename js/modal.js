7b66cc2cdc9fc31c706cb30ef62cbe7408d2172f
Fillip3505 <igorfilatov106@gmail.com>
2023-12-12 23:45:40 +0000

    added adaptability

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const openMenuBtn = document.querySelector('.open-menu');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
+openMenuBtn.addEventListener('click', toggleModal);
