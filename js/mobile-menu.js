587a1f1a8d07b5492c05264da4c8168de1a8055e
Fillip3505 <igorfilatov106@gmail.com>
2023-12-18 23:30:56 +0000

    fineshed project

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,24 @@
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuBtnOpen = document.querySelector(".menu-btn-open");
-const menuBtnClose = document.querySelector(".menu-btn-close");
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const menuBtnOpen = document.querySelector(".menu-btn-open");
+  const menuBtnClose = document.querySelector(".menu-btn-close");
+  const mobileMenuList = document.querySelector('.mobile-menu__list');
+  const mobileMenuLinks = mobileMenuList.querySelectorAll('.mobile-menu__link');
 
-const toggleMenu = () => mobileMenu.classList.toggle("is-open");
-const disableScroll = () =>
-  document.body.classList.toggle("is-scroll-disabled");
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("is-open");
+    disableScroll();
+  };
 
-menuBtnOpen.addEventListener("click", toggleMenu);
-menuBtnClose.addEventListener("click", toggleMenu);
+  const disableScroll = () =>
+    document.body.classList.toggle("is-scroll-disabled");
 
-menuBtnOpen.addEventListener("click", disableScroll);
-menuBtnClose.addEventListener("click", disableScroll);
+  menuBtnOpen.addEventListener("click", toggleMenu);
+  menuBtnClose.addEventListener("click", toggleMenu);
+
+  mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      toggleMenu();
+    });
+  });
+});
